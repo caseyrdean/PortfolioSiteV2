b434a11987143ca6ce63036a695eb929b6539ac6
caseyrdean <casey.r.dean1990@gmail.com>
2025-06-07 16:21:28 -0400

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,13 +5,21 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
 
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28 mb-28 max-w-[53rem]">
-      <SectionHeading>My projects</SectionHeading>
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[53rem] scroll-mt-28 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="projects"
+    >
+      <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
@@ -19,6 +27,6 @@ export default function Projects() {
           </React.Fragment>
         ))}
       </div>
-    </section>
+    </motion.section>
   );
 }
